241dd1c3ec19d6d1f32ffbcb69ce3c2523023dce
musin2 <muhsindaud75@gmail.com>
2024-09-05 20:43:11 +0300

    adds lab solution

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,38 @@
 #!/usr/bin/env python3
 
 class MyString:
-  pass
+  
+    def __init__(self, value = "") -> None:
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+  
+    @value.setter
+    def value(self, val):
+        if not isinstance(val, str):
+            print("The value must be a string.")
+        else:
+            self._value = val
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+    
+    def is_question(self):
+        return self.value.endswith("?")
+    
+    def is_exclamation(self):
+        return self.value.endswith("!")
+    
+    def count_sentences(self):
+        # Replace "!" and "?" with "."
+        sen = self.value.replace("!",".").replace("?",".")
+        # Split the string by "."
+        sentences = sen.split(".")
+        # Remove empty elements
+        sentences = [sentence for sentence in sentences if sentence.strip()]
+        return len(sentences)
+
+str1 = MyString("This, well, is a sentence. This is too!! And so is this, I think? Woo...")
+str1.count_sentences()
